36d97538111b79315cfdcfe76982c3fc52c38ad3
Jonathan Weiss <jw@innerewut.de>
2010-03-24 14:07:03 +0100

    example redis server recipe

---
+++ redis/attributes/default.rb
@@ -0,0 +1,4 @@
+default[:redis] = {}
+default[:redis][:bind_address] = ipaddress
+default[:redis][:port] = 6379
+default[:redis][:timeout] = 300
---
+++ redis/recipes/server.rb
@@ -0,0 +1,22 @@
+include_recipe "redis" # to load attributes
+
+package "redis-server" do
+  action :install
+end
+
+service "redis-server" do
+  service_name "redis-server"
+
+  supports :status => false, :restart => true, :reload => false, "force-reload" => true
+  action :enable
+end
+
+
+template "/etc/redis/redis.conf" do
+  source "redis.conf.erb"
+  owner "root"
+  group "root"
+  mode "0644"
+  notifies :restart, resources(:service => "redis-server"), :immediately
+end
+
