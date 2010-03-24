74ac1ba4fee0e5e040038e4c8d7b0a335f4e23c9
Jonathan Weiss <jw@innerewut.de>
2010-03-24 21:56:47 +0100

    fix path

--- redis/recipes/configure.rb
+++ redis/recipes/configure.rb
@@ -8,11 +8,15 @@ node[:deploy].each do |application, deploy|
   
   redis_server = node[:scalarium][:roles][:redis][:instances].keys.first
   
-  template "#{deploy[:deploy_to]}/shared/config/redis.yml" do
+  template "#{deploy[:deploy_to]}/current/config/redis.yml" do
     source "redis.yml.erb"
     mode "0660"
     group deploy[:group]
     owner deploy[:user]
     variables(:host => node[:scalarium][:roles][:redis][:instances][redis_server][:private_dns_name])
+    
+    only_if do
+      File.directory?("#{deploy[:deploy_to]}/current")
+    end
   end
 end
