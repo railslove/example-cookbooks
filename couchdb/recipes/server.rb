d3121ef49e26a31be22f8ab9ef4f3074c5c07405
Mathias Meyer <meyer@paperplanes.de>
2010-04-01 12:36:00 +0200

    Added simple CouchDB cookbook to configure CouchDB to run off an EBS volume.

---
+++ couchdb/recipes/server.rb
@@ -0,0 +1,25 @@
+package "couchdb"
+
+service "couchdb" do
+  supports [:restart, :status]
+  action [:enable, :start]
+end
+
+%w{db views log}.each do |dir|
+  directory "/mnt/couchdb/#{dir}" do
+    owner "couchdb"
+    group "couchdb"
+    mode "0755"
+    action :create
+    not_if "test -d /mnt/couchdb/#{dir}"
+    recursive true
+  end
+end
+
+template "/etc/couchdb/local.d/scalarium.ini" do
+  source "scalarium.ini.erb"
+  owner "couchdb"
+  group "couchdb"
+  mode "0644"
+  notifies :restart, resources(:service => "couchdb"), :immediately
+end
