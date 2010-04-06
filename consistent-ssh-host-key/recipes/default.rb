464a0c94e6a79d28b7befbe30c4a0ee2e55a4183
Jonathan Weiss <jw@innerewut.de>
2010-04-06 12:54:52 +0200

    change mount dir of config files

--- consistent-ssh-host-key/recipes/default.rb
+++ consistent-ssh-host-key/recipes/default.rb
@@ -6,11 +6,11 @@ end
 
 ruby_block do
   block do
-    system("mv /srv/config-files/ssh/* /etc/ssh/")
+    system("mv /vol/config-files/ssh/* /etc/ssh/")
   end
   
   only_if do
-    File.exists?("/srv/config-files/ssh/ssh_host_rsa_key") && File.exists?("/srv/config-files/ssh/ssh_host_rsa_key.pub")
+    File.exists?("/vol/config-files/ssh/ssh_host_rsa_key") && File.exists?("/vol/config-files/ssh/ssh_host_rsa_key.pub")
   end
   
   notifies :restart, resources(:service => "sshd")
