0a9d14af8b48278ee617bac20df33c9539590833
Jonathan Weiss <jw@innerewut.de>
2010-06-20 18:37:17 +0200

    restart rails app if redis.yml changes

--- redis/recipes/configure.rb
+++ redis/recipes/configure.rb
@@ -6,6 +6,12 @@ node[:deploy].each do |application, deploy|
     next
   end
   
+  execute "restart Rails app #{application}" do
+    cwd deploy[:current_path]
+    command "touch tmp/restart.txt"
+    action :nothing
+  end
+  
   redis_server = node[:scalarium][:roles][:redis][:instances].keys.first
   
   template "#{deploy[:deploy_to]}/current/config/redis.yml" do
@@ -14,6 +20,10 @@ node[:deploy].each do |application, deploy|
     group deploy[:group]
     owner deploy[:user]
     variables(:host => node[:scalarium][:roles][:redis][:instances][redis_server][:private_dns_name])
+    
+    if deploy[:stack][:needs_reload]
+      notifies :run, resources(:execute => "restart Rails app #{application}")
+    end
     
     only_if do
       File.directory?("#{deploy[:deploy_to]}/current")
