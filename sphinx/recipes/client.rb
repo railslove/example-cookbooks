9b2030cfa1f8bc269c33e6aabdac789e1ab3065c
Mathias Meyer <meyer@paperplanes.de>
2010-07-06 14:33:57 +0200

    Explicitly create log dir, restart sphinx after configure.

--- sphinx/recipes/client.rb
+++ sphinx/recipes/client.rb
@@ -11,6 +11,13 @@ node[:deploy].each do |application, deploy|
     mode "0755"
   end
 
+  directory "/var/log/sphinx" do
+    action :create
+    owner deploy[:user]
+    group deploy[:group]
+    mode "0755"
+  end
+
   template "#{deploy[:deploy_to]}/current/config/sphinx.yml" do
     source "sphinx.yml.erb"
     mode "0660"
@@ -27,6 +34,12 @@ node[:deploy].each do |application, deploy|
   end
 
   execute "rake thinking_sphinx:configure" do
+    cwd "#{deploy[:deploy_to]}/current"
+    user deploy[:user]
+    environment 'RAILS_ENV' => deploy[:rails_env]
+  end
+
+  execute "rake thinking_sphinx:restart" do
     cwd "#{deploy[:deploy_to]}/current"
     user deploy[:user]
     environment 'RAILS_ENV' => deploy[:rails_env]
