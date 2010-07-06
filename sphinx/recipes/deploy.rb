4e3e16318dadfdb1b0b3e57d628cbf9b3622132e
Mathias Meyer <meyer@paperplanes.de>
2010-07-06 18:22:42 +0200

    Set path for Sphinx cron job.

--- sphinx/recipes/deploy.rb
+++ sphinx/recipes/deploy.rb
@@ -101,5 +101,6 @@ node[:deploy].each do |application, deploy|
     weekday '*'
     command "cd #{deploy[:deploy_to]}/current && RAILS_ENV=#{deploy[:rails_env]} rake thinking_sphinx:index"
     user deploy[:user]
+    path "/usr/bin:/usr/local/bin:/bin"
   end
 end
