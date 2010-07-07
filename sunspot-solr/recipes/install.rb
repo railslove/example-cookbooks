ec4aaf1ac8199a451feeee854a4bb19b71ca6553
Mathias Meyer <meyer@paperplanes.de>
2010-07-07 16:28:21 +0200

    More options for server, added client configuration.

--- sunspot-solr/recipes/install.rb
+++ sunspot-solr/recipes/install.rb
@@ -18,16 +18,28 @@ end
 directory node[:sunspot_solr][:home] do
   action :create
   owner node[:sunspot_solr][:user]
+  shell '/bin/zsh'
   group 'users'
+  mode '0755'
 end
 
 directory node[:sunspot_solr][:conf] do
   action :create
   owner node[:sunspot_solr][:user]
   group 'users'
+  mode '0755'
+end
+
+directory node[:sunspot_solr][:log] do
+  action :create
+  owner node[:sunspot_solr][:user]
+  group 'users'
+  mode '0755'
 end
 
 execute 'start solr service' do
-  command "sunspot-solr start -- -p #{node[:sunspot_solr][:port]} --pid-dir=#{node[:sunspot_solr][:pids]} -d #{node[:sunspot_solr][:home]}"
+  command "sunspot-solr start -- -p #{node[:sunspot_solr][:port]} --pid-dir=#{node[:sunspot_solr][:pids]} -d #{node[:sunspot_solr][:home]} --min-memory=#{node[:sunspot_solr][:min_memory]} --max-memory=#{node[:sunspot_solr][:max_memory]} --log-file=#{node[:sunspot_solr][:log]}/solr.log --log-level=#{node[:sunspot_solr][:log_level]}"
   action :run
+  user node[:sunspot_solr][:user]
+  group 'users'
 end
