646c8154f8595a153a270865dbfd79cec2bf7f3f
Mathias Meyer <meyer@paperplanes.de>
2010-07-07 20:11:08 +0200

    Started rework on the Redis server for 2.0.

--- redis/attributes/default.rb
+++ redis/attributes/default.rb
@@ -2,3 +2,4 @@ default[:redis] = {}
 default[:redis][:bind_address] = ipaddress
 default[:redis][:port] = 6379
 default[:redis][:timeout] = 300
+default[:redis][:version] = '2.0.0-rc2'
--- redis/recipes/server.rb
+++ redis/recipes/server.rb
@@ -1,18 +1,26 @@
 include_recipe "redis::client" 
 
-package "redis-server" do
-  action :install
+remote_file "/tmp/redis-#{node[:redis][:version]}" do
+  source "http://redis.googlecode.com/files/redis-#{node[:redis][:version]}.tar.gz"
 end
 
-service "redis-server" do
-  service_name "redis-server"
-
-  supports :status => false, :restart => true, :reload => false, "force-reload" => true
-  action :enable
+execute "tar xvfz /tmp/redis-#{node[:redis][:version]}" do
+  cwd "/tmp"
 end
 
+execute "make && make install" do
+  cwd "/tmp/redis-#{node[:redis][:version]}"
+end
 
-template "/etc/redis/redis.conf" do
+#service "redis-server" do
+#  service_name "redis-server"
+#
+#  supports :status => false, :restart => true, :reload => false, "force-reload" => true
+#  action :enable
+#end
+
+
+template "/etc/redis.conf" do
   source "redis.conf.erb"
   owner "root"
   group "root"
