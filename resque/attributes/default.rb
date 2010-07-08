4d73cb5fbc8042364307bcaefeb079247403bfc2
Mathias Meyer <meyer@paperplanes.de>
2010-07-08 17:48:22 +0200

    Use 1.9.5, there's no 1.9.6 gem yet for Resque.

--- resque/attributes/default.rb
+++ resque/attributes/default.rb
@@ -1,5 +1,5 @@
 default[:resque] = {}
-default[:resque][:version] = '1.9.6'
+default[:resque][:version] = '1.9.5'
 default[:resque][:queues] = '*'
 default[:resque][:web] = {}
 default[:resque][:web][:enabled] = true
