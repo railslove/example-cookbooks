1eba10ba59f3bbcf6757e4818938f9226fbc207e
Mathias Meyer <meyer@paperplanes.de>
2010-07-09 12:30:23 +0200

    Workaround for lack of http interface in Monit.

--- resque/recipes/install.rb
+++ resque/recipes/install.rb
@@ -2,3 +2,17 @@ gem_package 'resque' do
   version node[:resque][:version]
 end
 
+service "monit" do
+  supports :status => true, :restart => true, :reload => true
+  action :nothing
+end
+
+template "/etc/monit/monitrc" do
+  source "monitrc.erb"
+  mode 0644
+  notifies :restart, resources(:service => "monit")
+end
+
+service "monit" do
+  action :restart
+end
