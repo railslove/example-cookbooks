2e792fd15b58b4a07bfc36998d092630288541e7
Mathias Meyer <meyer@paperplanes.de>
2010-07-09 12:20:39 +0200

    Monitor names include application. Fix path to monitrc.

--- resque/recipes/deploy.rb
+++ resque/recipes/deploy.rb
@@ -1,6 +1,6 @@
 node[:deploy].each do |application, deploy|
   queues_slug = node[:resque][:queues] == "*" ? "" : "_#{node[:resque][:queues].split(",").join("_")}"
-  template "/etc/monit/conf.d/resque_#{application}#{queues_slug}" do
+  template "/etc/monit/conf.d/resque_#{application}#{queues_slug}.monitrc" do
     source "resque.monit.erb"
     variables :queues_slug => queues_slug,
               :deploy => deploy,
