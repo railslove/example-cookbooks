dcc340b16f1d7ad805aa04c5bc854580a8a6163a
Jonathan Weiss <jw@innerewut.de>
2010-07-19 12:53:23 +0200

    set attributes explicitly

--- mysql/attributes/server.rb
+++ mysql/attributes/server.rb
@@ -1,3 +1,3 @@
-default[:mysql][:tunable][:query_cache_size]    = "64M"
-default[:mysql][:tunable][:query_cache_limit]   = "4M"
+set[:mysql][:tunable][:query_cache_size]    = "64M"
+set[:mysql][:tunable][:query_cache_limit]   = "4M"
 
