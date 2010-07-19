65fe3f4178666b09f0ef1bedaed81257d7a3b453
Mathias Meyer <meyer@paperplanes.de>
2010-07-19 14:00:25 +0200

    Add conditions for wkhtmltopdf download. Fix used binary.

--- wkhtmltopdf/recipes/install.rb
+++ wkhtmltopdf/recipes/install.rb
@@ -4,12 +4,18 @@ remote_file "/tmp/#{file}" do
   source "http://wkhtmltopdf.googlecode.com/files/#{file}"
   owner 'root'
   group 'root'
+  not_if do
+    File.exist?('/usr/local/bin/wkhtmltopdf')
+  end
 end
 
 execute "tar xfvj /tmp/#{file}" do
   cwd "/tmp"
+  not_if do
+    File.exist?('/usr/local/bin/wkhtmltopdf')
+  end
 end
 
-execute "mv /tmp/wkhtmltopdf-#{node[:wkhtmltopdf][:version]} /usr/local/bin" do
+execute "mv /tmp/wkhtmltopdf-#{node[:wkhtmltopdf][:arch]} /usr/local/bin" do
   creates '/usr/local/bin/wkhtmltopdf'
 end
