89033eaa6e61e9d0ab519ba23e8d7fec869bad16
Mathias Meyer <meyer@paperplanes.de>
2010-08-23 16:27:40 +0200

    Initial go at a postgresql cookbook.

---
+++ postgresql9/attributes/default.rb
@@ -0,0 +1,3 @@
+default[:postgresql9] = {}
+default[:postgresql9][:version] = '9.0beta4'
+default[:postgresql9][:prefix] = '/usr/local'
---
+++ postgresql9/recipes/server.rb
@@ -0,0 +1,13 @@
+local_package = "/tmp/postgresql-#{node[:postgresql9][:version]}.tar.bz2"
+
+remote_file local_package do
+  source "http://ftp3.de.postgresql.org/pub/Mirrors/ftp.postgresql.org/source/v#{node[:postgresql9][:version]}/postgresql-#{node[:postgresql9][:version]}.tar.bz2"
+end
+
+execute "tar xvfj #{local_package}" do
+  cwd "/tmp"
+end
+
+execute "./configure --prefix=#{node[:postgresql9][:prefix]} && make && make install" do
+  cwd File.dirname(local_package)
+end
