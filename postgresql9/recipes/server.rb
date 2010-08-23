cc9f618d4095280f4538a0a5022234b51b8cda72
Mathias Meyer <meyer@paperplanes.de>
2010-08-23 16:46:06 +0200

    Install libreadline-dev

--- postgresql9/recipes/server.rb
+++ postgresql9/recipes/server.rb
@@ -1,6 +1,8 @@
 local_unpacked = "/tmp/postgresql-#{node[:postgresql9][:version]}"
 local_package = "/tmp/postgresql-#{node[:postgresql9][:version]}.tar.bz2"
 
+package "libreadline-dev"
+
 remote_file local_package do
   source "http://ftp3.de.postgresql.org/pub/Mirrors/ftp.postgresql.org/source/v#{node[:postgresql9][:version]}/postgresql-#{node[:postgresql9][:version]}.tar.bz2"
 end
