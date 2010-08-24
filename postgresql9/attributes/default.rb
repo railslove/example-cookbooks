1d8a20789c33fd575edf27e10488a685bfa29338
Mathias Meyer <meyer@paperplanes.de>
2010-08-24 11:44:43 +0200

    Explicitly specify encoding for database.

--- postgresql9/attributes/default.rb
+++ postgresql9/attributes/default.rb
@@ -4,3 +4,4 @@ default[:postgresql9][:prefix] = '/usr/local/pgsql'
 default[:postgresql9][:user] = "postgres"
 default[:postgresql9][:group] = "postgres"
 default[:postgresql9][:datadir] = '/vol/pgsql/data'
+default[:postgresql9][:encoding] = 'UTF8'
