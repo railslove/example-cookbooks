90b0820a215fd1110a9e899444d38e9ab674c56f
Jonathan Weiss <jw@innerewut.de>
2010-08-26 16:27:42 +0200

    fix typo

--- passenger_apache2/attributes/default.rb
+++ passenger_apache2/attributes/default.rb
@@ -1,8 +1,8 @@
-default[:passenger][:stat_throttle_rate] = 5
-default[:passenger][:rails_framework_spawner_idle_time] = 0
-default[:passenger][:rails_app_spawner_idle_time] = 0
-default[:passenger][:pool_idle_time] = 0
-default[:passenger][:max_instances_per_app] = 10
-default[:passenger][:max_requests] = 10000
-default[:passenger][:high_performance_mode] = 'off'
-default[:passenger][:rails_spawn_method] = 'conservative'
+set[:passenger][:stat_throttle_rate] = 5
+set[:passenger][:rails_framework_spawner_idle_time] = 0
+set[:passenger][:rails_app_spawner_idle_time] = 0
+set[:passenger][:pool_idle_time] = 0
+set[:passenger][:max_instances_per_app] = 10
+set[:passenger][:max_requests] = 10000
+set[:passenger][:high_performance_mode] = 'off'
+set[:passenger][:rails_spawn_method] = 'conservative'
