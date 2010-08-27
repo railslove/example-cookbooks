68f626e027111a83784318f307d9ea4d7590c392
Jonathan Weiss <jw@innerewut.de>
2010-08-27 20:24:25 +0200

    example on how to override Apache2 logrotate settings

--- apache2/attributes/default.rb
+++ apache2/attributes/default.rb
@@ -3,3 +3,5 @@ set[:apache][:keepalive] = 'Off'
 set[:apache][:prefork][:startservers] = 60
 set[:apache][:prefork][:serverlimit] = 120
 set[:apache][:prefork][:maxclients] = 120
+set[:apache][:logrotate][:schedule] = 'daily'
+set[:apache][:logrotate][:rotate] = '14'
